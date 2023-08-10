5768fb8a34b900dc95515b6e5cd74a51d6331cfb
Cedric Ally <cedric.ally@cegeka.com>
2023-08-10 11:41:52 +0200

    Implement Tooltip on Advanced Settings

--- DataRestorationTool/Properties/Resources.Designer.cs
+++ DataRestorationTool/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace DataRestorationTool.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap info {
+            get {
+                object obj = ResourceManager.GetObject("info", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Load {
             get {
                 object obj = ResourceManager.GetObject("Load", resourceCulture);
